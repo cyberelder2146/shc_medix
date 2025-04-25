1923957f96cdf56ae8a99f7b7824609d1d551f49
root <root@Abdoul.localdomain>
2025-04-25 15:30:41 +0100

    indent_Err

--- app.py
+++ app.py
@@ -389,7 +389,6 @@ def doctor_chat(patient_id):
     )
 
 if __name__ == '__main__':
-     with app.app_context():
-         db.create_all()
-    # migrate = Migrate(app, db)
+    with app.app_context():
+        db.create_all()
     app.run(host="0.0.0.0", port=5000)
