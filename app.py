b5d3af743fc459482c8a3e65bd277d36c012a99a
root <root@Abdoul.localdomain>
2025-04-25 14:57:35 +0100

    host binding update

--- app.py
+++ app.py
@@ -392,4 +392,4 @@ if __name__ == '__main__':
     # with app.app_context():
         # db.create_all()
     # migrate = Migrate(app, db)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
